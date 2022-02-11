5cf317d41eb743464a0c281eaccab258c353b557
Alecto Irene Perez <perez.cs@pm.me>
2022-02-11 00:07:09 -0700

    Add current(), pop_current() to arg_view
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/arglet/arg_view.hpp
+++ include/arglet/arg_view.hpp
@@ -11,7 +11,7 @@ using std::string_view;
 class arg_view {
     char const** start_ {nullptr};
     char const** end_ {nullptr};
-    token current {};
+    token current_arg {};
 
     arg_view(char const** Start, char const** End) noexcept
       : start_(Start)
@@ -34,7 +34,7 @@ class arg_view {
 
         // We only have a current argument if argc > 0
         if (argc > 0) {
-            current = token(*argv);
+            current_arg = token(*argv);
         }
     }
     arg_view() = default;
@@ -43,39 +43,57 @@ class arg_view {
     // Pop the token at the front of the list of arguments. If the list of
     // arguments is empty, return an empty token (will evaluate to false whech
     // tested)
-    constexpr token pop() noexcept {
+    constexpr token pop_current() noexcept {
         // This will break if fullptr is passed in
         if (start_ + 1 < end_) {
             // Increment the start
             start_ += 1;
             // Replace the current token with the next token, and return the old
             // value
-            return util::exchange(current, token(*start_));
+            return util::exchange(current_arg, token(*start_));
         } else {
             start_ = end_;
             // Replace the current token with an empty token
-            return util::exchange(current, token());
+            return util::exchange(current_arg, token());
+        }
+    }
+
+    // Pop the token at the front of the list of arguments, returning void
+    constexpr void pop() noexcept {
+        // This will break if fullptr is passed in
+        if (start_ + 1 < end_) {
+            // Increment the start
+            start_ += 1;
+            // Replace the current token with the next token, and return the old
+            // value
+            current_arg = token(*start_);
+        } else {
+            start_ = end_;
+            // Replace the current token with an empty token
+            current_arg = token();
         }
     }
 
     // Return the token at the front of the list of arguments. Returns an empty
     // token if the list is empty.
-    constexpr token peek() const noexcept { return current; }
+    constexpr token peek() const noexcept { return current_arg; }
+
+    // Return the token at the front of the list of arguments. Returns an empty
+    // token if the list is empty.
+    constexpr token current() const noexcept { return current_arg; }
 
     // Checks if the current argument starts with a given character
     constexpr bool starts_with(char ch) const noexcept {
-        return current.starts_with(ch);
+        return current_arg.starts_with(ch);
     }
 
     // Checks if the current argument starts with the given string view
     constexpr bool starts_with(string_view sv) const noexcept {
-        return current.starts_with(sv);
+        return current_arg.starts_with(sv);
     }
 
     // Get the number of arguments in the view
-    constexpr size_t size() const noexcept {
-        return end_ - start_;
-    }
+    constexpr size_t size() const noexcept { return end_ - start_; }
 
     // Checks if the arg_view is empty (has 0 elements)
     constexpr bool empty() const noexcept { return start_ == end_; }
