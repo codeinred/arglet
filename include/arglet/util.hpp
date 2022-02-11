cc12878262fd06c18d936099a53830d9b31798fb
Alecto Irene Perez <perez.cs@pm.me>
2022-02-10 18:38:30 -0700

    Add util.hpp with noexcept exchange()
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

---
+++ include/arglet/util.hpp
@@ -0,0 +1,12 @@
+#include <type_traits>
+
+namespace arglet::util {
+template <class T>
+T exchange(T& value, T&& moved) noexcept(
+    std::is_nothrow_move_constructible_v<T>&&
+        std::is_nothrow_move_assignable_v<T>) {
+    T tmp = static_cast<T&&>(value);
+    value = static_cast<T&&>(moved);
+    return tmp;
+}
+} // namespace arglet::util
